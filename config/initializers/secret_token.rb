793d9e1e0da67019946b86f37040be688b7e98cd
Ian Gray <ian@graydiant.com>
2012-09-26 22:15:26 -0400

    installed rails, rvm 193 MRI for mongoid

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CheapTaps::Application.config.secret_token = '667df33472044e4780792098f019179d4c36d20195d7054dba1f65d6c5984cd078da32acdbb73dcf70b98c8cd2a503612382d2e76e67ecc68729936de5eedaa4'
